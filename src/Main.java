c912527514cf6de2be88714e8e5dde5597e9ae40
Antoni_Iwanowski <75222159+AntekIwa@users.noreply.github.com>
2026-04-04 00:27:48 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
                 new Agent("James", "Bond"),
                 new Czlowiek("Anna", "Nowak"),
                 new Agent("Darth", "Vader"),
-                new Czlowiek("Yoda", "Yoda") // Ten element nie zmieści się w początkowej tablicy
+                new Czlowiek("Yoda", "Yoda") 
         };
         for (Osoba o : nowiLudzie) {
             if (liczbaElementow >= tablica.length) {
@@ -31,50 +31,50 @@ public class Main {
     static void testUlamkow(){
 
        Ulamek u1 = new Ulamek(4, 8);
-       System.out.println("4/8 po skroceniu: " + u1); // Oczekiwane: 1 / 2
+       System.out.println("4/8 po skroceniu: " + u1); 
 
        Ulamek u2 = new Ulamek(2, 1);
-       System.out.println("2/1 po skroceniu: " + u2); // Oczekiwane: 2
+       System.out.println("2/1 po skroceniu: " + u2); 
 
        Ulamek u3 = new Ulamek(0, 5);
-       System.out.println("0/5 po skroceniu: " + u3); // Oczekiwane: 0
+       System.out.println("0/5 po skroceniu: " + u3); 
 
 
        // Dodawanie
        Ulamek dodawanie1 = new Ulamek(1, 2);
        Ulamek dodawanie2 = new Ulamek(1, 3);
        dodawanie1.dodaj(dodawanie2);
-       System.out.println("1/2 + 1/3 = " + dodawanie1); // Oczekiwane: 5 / 6
+       System.out.println("1/2 + 1/3 = " + dodawanie1); 
 
        // Dodawanie liczby calkowitej
        Ulamek dodawanieLiczby = new Ulamek(1, 2);
        dodawanieLiczby.dodaj(2);
-       System.out.println("1/2 + 2 = " + dodawanieLiczby); // Oczekiwane: 5 / 2
+       System.out.println("1/2 + 2 = " + dodawanieLiczby); 
 
        // Odejmowanie
        Ulamek odejmowanie1 = new Ulamek(3, 4);
        Ulamek odejmowanie2 = new Ulamek(1, 4);
        odejmowanie1.odejmij(odejmowanie2);
-       System.out.println("3/4 - 1/4 = " + odejmowanie1); // Oczekiwane: 1 / 2
+       System.out.println("3/4 - 1/4 = " + odejmowanie1); 
 
        // mnozenie
        Ulamek mnozenie1 = new Ulamek(2, 3);
        Ulamek mnozenie2 = new Ulamek(3, 4);
        mnozenie1.pomnoz(mnozenie2);
-       System.out.println("2/3 * 3/4 = " + mnozenie1); // Oczekiwane: 1 / 2
+       System.out.println("2/3 * 3/4 = " + mnozenie1); 
 
        // Dzielenie
        Ulamek dzielenie1 = new Ulamek(1, 2);
        Ulamek dzielenie2 = new Ulamek(1, 4);
        dzielenie1.podziel(dzielenie2);
-       System.out.println("1/2 / 1/4 = " + dzielenie1); // Oczekiwane: 2
+       System.out.println("1/2 / 1/4 = " + dzielenie1); 
 
         // liczby ujemne
        Ulamek ujemny = new Ulamek(1, -2);
-       System.out.println("1/-2 po utworzeniu: " + ujemny); // Zależnie od implementacji skracania znaków
+       System.out.println("1/-2 po utworzeniu: " + ujemny); 
 
        Ulamek ujemny2 = new Ulamek(-1, 2);
        ujemny.dodaj(ujemny2);
-       System.out.println("-1/2 + (-1/2) = " + ujemny); // Oczekiwane: -1
+       System.out.println("-1/2 + (-1/2) = " + ujemny); 
    }
 }
