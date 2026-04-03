d02a82a24a3739afec6029f4c25d3a665d854960
Adam Jankowski <ap.jankowski@student.uw.edu.pl>
2026-04-03 18:26:34 +0200

    Implemented dynamic FIFO queue

--- src/BuforCykliczny.java
+++ src/BuforCykliczny.java
@@ -1,18 +1,67 @@
 import Ludzie.Osoba;
 
 public class BuforCykliczny {
-    public static Osoba[] powieksz(Osoba[] bufor){
-        Osoba[] nowy = new Osoba[bufor.length*2];
-        for(int i = 0; i < bufor.length; i++){
-            nowy[i] = bufor[i];
-        }
-        return nowy;
+
+    private Osoba[] tablica = new Osoba [4];
+    private int pojemnosc = 4;
+    private int rozmiar = 0;
+    private int poczatek = 0;
+
+    private void powieksz()
+    {
+        Osoba[] nowy = new Osoba[pojemnosc * 2];
+
+        for(int i = 0; i < rozmiar; i++)  
+            nowy[i] = tablica[(poczatek + i) % pojemnosc];
+
+        pojemnosc *= 2;
+        poczatek = 0;
+        tablica = nowy;
     }
-    public static Osoba[] pomniejsz(Osoba[] bufor){
-        Osoba[] nowy = new Osoba[bufor.length/2];
-        for(int i = 0; i < bufor.length/4; i++){
-            nowy[i] = bufor[i];
+
+    private void pomniejsz()
+    {
+        Osoba[] nowy = new Osoba[pojemnosc / 2];
+
+        for(int i = 0; i < rozmiar; i++)
+            nowy[i] = tablica[(poczatek + i) % pojemnosc];
+
+        pojemnosc /= 2;
+        poczatek = 0;
+        tablica = nowy;
+    }
+
+    public boolean pusty()
+    {
+        return rozmiar == 0;
+    }
+
+    public void dodaj(Osoba elem)
+    {
+        if(rozmiar == pojemnosc) powieksz();
+        tablica[(poczatek + rozmiar) % pojemnosc] = elem;
+        rozmiar++;
+    }
+
+    public Osoba pobierz()
+    {
+        if(pusty()) return null;
+        Osoba o = tablica[poczatek];
+        rozmiar--;
+        poczatek = (poczatek + 1)  % pojemnosc;
+        if(pojemnosc > 8 && rozmiar <= pojemnosc / 4) pomniejsz();
+        return o;
+    }
+
+    public void wypisz()
+    {
+        String wynik = "[";
+        for(int i = 0; i < rozmiar - 1 ; i++)
+        {
+            wynik += tablica[(poczatek + i) % pojemnosc] + ",";
         }
-        return nowy;
+        if(rozmiar > 0) wynik += tablica[(poczatek + rozmiar - 1) % pojemnosc];
+        wynik += "]";
+        System.out.println(wynik);
     }
 }
